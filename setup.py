2f9dc0fa12a49ca762560061c3dc6b0cf0cc5976
coballejr <55487608+coballejr@users.noreply.github.com>
2019-10-26 15:32:08 -0500

    Fixed version in setup.py.

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 # setup.py 
 
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'bayes_tda',
   packages = ['bayes_tda'],   
   version = '0.2',      
   license='MIT',        
   description = 'Point process model for Bayesian inference with persistence diagrams.',
-  long_description = README.md,
+  long_description = long_description,
   long_description_content_type = 'text/markdown',
   author = 'Christopher Oballe',
   author_email = 'coballejr@gmail.com',      
   url = 'https://github.com/coballejr/bayes_tda/',
-  download_url = 'https://github.com/coballejr/bayes_tda/archive/v_01.tar.gz',
+  download_url = 'https://github.com/coballejr/bayes_tda/archive/v_02.tar.gz',
   keywords = ['Persistent Homology', 'Topological Data Analysis', 'Bayesian','Point process', 'Poisson process'],   # Keywords that define your package best
   install_requires=[            
           'numpy',
